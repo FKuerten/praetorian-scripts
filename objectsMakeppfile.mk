6884fd7a32c58e1dc0800b571e58c79042bf6510
Fabian <Fabian.Kuerten@rwth-aachen.de>
2014-02-10 15:41:20 +0100

    improve copyright stuff

--- objectsMakeppfile.mk
+++ objectsMakeppfile.mk
@@ -1,3 +1,20 @@
+#
+#   Copyright 2014 Fabian "Praetorian" Kürten
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 CREATE_DIR:=$(shell if [ ! -e target/objects ]; then mkdir -p target/objects; echo "mkdir -p target/objects"; fi)
 
 # This recreates the Makeppfile responsible for building the objects
--- version.mk
+++ version.mk
@@ -1,3 +1,21 @@
+#
+#   Copyright 2014 Fabian "Praetorian" Kürten
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 # This is responsible for creating a version header
 target/generated/version.h++: target/generated/version.h++.auto
     @if [ ! -e target/generated ]; then mkdir -p target/generated; echo "mkdir -p target/generated"; fi
