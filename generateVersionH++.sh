7a07831ffcc95546b2c3ebbcdd76daeb0e53c4a4
Fabian <Fabian.Kuerten@rwth-aachen.de>
2014-02-10 15:40:54 +0100

    small fix regarding version creation

--- generateVersionH++.sh
+++ generateVersionH++.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
-echo -n "#define TYRANT_CACHE_VERSION \""
+PREFIX="$1"
+if [ -z "${PREFIX}"] ; then
+    echo "Need to specify a prefix to create a version header." >&2
+    exit 1
+fi
+
+echo -n "#define ${PREFIX}_VERSION \""
 git rev-parse HEAD | tr --delete "\n"
 echo  "\""
-echo -n "#define TYRANT_CACHE_VERSION_TAGS \""
+echo -n "#define ${PREFIX}_VERSION_TAGS \""
 git tag --points-at HEAD | tr "\n" " "
 echo  "\""
-echo -n "#define TYRANT_CACHE_VERSION_DESCRIBE \""
+echo -n "#define ${PREFIX}_VERSION_DESCRIBE \""
 git describe HEAD | tr --delete "\n"
 echo "\""
 #
-echo -n "#define TYRANT_CACHE_DIRTY_HEAD "
+echo -n "#define ${PREFIX}_DIRTY_HEAD "
 if [ `git diff HEAD | wc --chars` -gt 0 ]; then 
     echo true
 else \
     echo false
 fi
-echo -n "#define TYRANT_CACHE_DIRTY_HASH \""
+echo -n "#define ${PREFIX}_DIRTY_HASH \""
 git diff HEAD | sha1sum | cut -d " " -f 1 | tr -d "\n"
 echo  "\""
 #
